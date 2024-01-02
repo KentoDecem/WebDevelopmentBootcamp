600c9ec67f7571f2f5f683175122d05eb946a318
Kento <72524097+KentoDecem@users.noreply.github.com>
2024-01-02 10:13:04 +0100

    Create index.js

---
+++ Section 15/index.js
@@ -0,0 +1,61 @@
+// var n = Math.random();
+// n = n * 6;
+// n = Math.floor(n) + 1;
+// console.log(n);
+
+// Love Calculator
+// prompt("What is your name?");
+// prompt("What is your lover's name?");
+
+// var percentage = Math.random();
+// percentage = Math.floor(percentage * 100) + 1;
+
+// if (percentage > 70) {
+//     alert("Your chances are: " + percentage + "%! Go for it...");
+// }
+
+// if (percentage > 30 && percentage <= 70) {
+//     alert("Your chances are: " + percentage + "%");
+// }
+    
+// else {
+//     alert("Your chances are: " + percentage + "%! Mehhh. Really?");
+// }
+
+
+// var guestList = ["Angela", "Jack", "Pam", "James", "Lara", "Jason"];
+// var guest = prompt("What is your name?");
+// if (guestList.includes(guest)) {
+//     alert("Welcome!")
+// }
+
+// var output = [];
+// var num = 1;
+// var word = "";
+
+// function fizzBuzz() {
+//     while (num <= 100) {
+    
+//     if (num % 3 == 0) word += "Fizz";
+//     if (num % 5 == 0) word += "Buzz";
+//     output.push(word || num);
+    
+//     num++;
+//     word="";
+    
+
+//     }
+//     console.log(output);
+// }
+
+// function beer() {
+//     var num = 99;
+//     while (num > 1) {
+//         console.log(num + " bottles of beer on the wall, " + num + " bottles of beer." + " Take one down and pass it around, " + (num - 1) + " bottles of beer on the wall.")
+//         num--;
+//     }
+
+//     console.log(num + " bottles of beer on the wall, " + num + " bottles of beer." + " Take one down and pass it around," + " no more bottles of beer on the wall.")
+//     console.log("No more bottles of beer on the wall, no more bottles of beer." + " Go to the store and buy some more, 99 bottles of beer on the wall.")
+// }
+
