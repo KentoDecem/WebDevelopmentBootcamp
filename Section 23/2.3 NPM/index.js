e9b68c85239ce5d79dd4c45ce12525b75aceb531
Kento <72524097+KentoDecem@users.noreply.github.com>
2024-01-16 14:08:31 +0100

    Add files via upload

---
+++ Section 23/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+import generateName from 'sillyname';
+import superheroes from "superheroes";
+// var sillyName = generateName();
+// console.log(`My name is ${sillyName}`);
+
+var superheroesName = superheroes.random();
+//console.log(`My name is ${superheroesName}!`)
+console.log(superheroes.all);
